bc5d394044ec9cc65761808a0b69c44fa124a2fb
Piotr Sobol <piotr.dominik.sobol@gmail.com>
2026-05-15 15:26:39 +0200

    Create categories route

---
+++ app/consts/categories.ts
@@ -0,0 +1,11 @@
+export const categories = [
+    "healthcare.hospital",
+    "healthcare.pharmacy",
+    "commercial.supermarket",
+    "commercial.shopping_mall",
+    "commercial.restaurant",
+    "commercial.cafe",
+    "commercial.bar",
+    "commercial.hotel",
+    "sport.fitness.gym",
+]
