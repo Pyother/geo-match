309bf48c9989cad2a0925bd9d9747b452195f3dc
Piotr Sobol <piotr.dominik.sobol@gmail.com>
2026-05-15 21:23:52 +0200

    Add boundaries route

--- app/api/place/[placeId]/categories/[categories]/route.ts
+++ app/api/place/[placeId]/categories/[categories]/route.ts
@@ -3,11 +3,11 @@ import { NextResponse } from "next/server";
 export async function GET(
     _request: Request,
     { params }: { params: Promise<{ 
-        id: string,
+        placeId: string,
         categories: string
     }> }
 ) {
-    const { id: placeId, categories: categoriesString } = await params;
+    const { placeId, categories: categoriesString } = await params;
     const categories = categoriesString.split(",");
     const apiKey = process.env.GEOAPIFY_API_KEY;
     const url = `https://api.geoapify.com/v2/places?categories=${categories.join(",")}&filter=place:${placeId}&limit=50&apiKey=${apiKey}`;
